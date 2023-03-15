f97a652224a5343e62010a0f3aee3143e57fee07
Scott Wilson <scott@propersquid.com>
2023-03-15 09:28:15 -0700

    Update bitflags to 2.0.0

--- src/check.rs
+++ src/check.rs
@@ -3,6 +3,7 @@ use std::borrow::Cow;
 bitflags::bitflags! {
     /// The check hint flags contains useful information such as whether the
     /// check should support auto-fixing issues.
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
     pub struct CheckHint: u8 {
         /// The check supports no extra features.
         ///
