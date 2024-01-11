e2678630a0737c33476d00d2b970f17203e73ba5
Scott Wilson <scott@propersquid.com>
2024-01-10 23:02:00 -0800

    More fixes, possibly making MacOS happy

---
+++ bindings/cpp/src/core.cpp
@@ -0,0 +1,10 @@
+extern "C"
+{
+#include <cchecks.h>
+}
+
+#include "cppchecks/check.h"
+#include "cppchecks/core.h"
+#include "cppchecks/item.h"
+#include "cppchecks/items.h"
+#include "cppchecks/status.h"
